9a6c7bee19d4ce5c7098e4276cc90639ff3f049b
Vladislav Serebriakov <vladislav.serebriakov@x2sy.com>
2022-03-22 13:01:26 +0200

    3. Add simple graphql query and mutations

--- src/index.js
+++ src/index.js
@@ -3,14 +3,46 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!,
+    note(id: ID!): Note!,
+  }
+  type Mutation {
+    newNote(content: String!): Note!,
+  }
+  type Note {
+    id: ID!,
+    content: String!,
+    author: String!,
   }
 `
+
 const resolvers = {
   Query: {
     hello: () => "Hello world",
+    notes: () => notes,
+    note: (parent, {id}) => notes.find(n => n.id === id),
+  },
+  Mutation: {
+    newNote: (parent, {content}) => {
+      console.log(content);
+      const note = {
+        id: String(notes.length + 1),
+        content,
+        author: 'Adam Scott',
+      }
+      notes.push(note)
+      return note
+    }
   }
 }
 
